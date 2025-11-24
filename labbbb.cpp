7e76b99e977640c5c647a044dbef7b39b43e8997
Дарина Приходько <Daryna.Prykhodko@cs.khpi.edu.ua>
2025-11-24 23:40:39 +0200

    UnitTests

--- labbbb.cpp
+++ labbbb.cpp
@@ -5,6 +5,86 @@
 #include <limits>
 using namespace std;
 
+
+void checkN(int n) {
+	if (n < 2) {
+		throw "Error: n must be bigger than 1!";
+	}
+}
+
+void checkRangeParams(double a, double b, double step) {
+	if (b <= a) {
+		throw "Error: b must be bigger than a!";
+	}
+	if (step <= 0) {
+		throw "Error: step must be bigger than 0!";
+	}
+}
+
+double checkValidInt(const char* ex) {
+	cout << ex;
+	int temp;
+	cin >> temp;
+	if (cin.fail() || cin.peek() != '\n') {
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		throw "Error: input correct data.";
+	}
+	else {
+		return temp;
+	}
+}
+
+double checkValidDouble(const char* ex) {
+	cout << ex;
+	double temp;
+	cin >> temp;
+	if (cin.fail() || cin.peek() != '\n') {
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		throw "Error: input correct data.";
+	}
+	else {
+		return temp;
+	}
+}
+
+double calcFunc1(int n, double x) {
+	double y = 0;
+	if (x < -3) {
+		for (int i = 1; i <= n; i++) {
+			y += (pow(i + x, 2)) / (2.0 * i);
+		}
+	}
+
+	else {
+		double mlt = 1.0;
+		for (int j = 1; j <= n - 1; j++) {
+			double sum = 0.0;
+			for (int i = 2; i <= n + 1; i++) {
+				double temp = pow(i + (double)i / j - j, 2);
+				sum = sum + temp;
+			}
+			mlt = mlt * sum;
+		}
+		y = mlt;
+	}
+	return y;
+}
+
+void calcFunc2(int n, double a, double b, double step) {
+	//Outputting results
+	int k = 1;
+	for (double x = a; x <= b; x += step) {
+		double y = calcFunc1(n, x);
+		cout << fixed << setprecision(5);
+		cout << "x" << k << "=" << x << setw(15) << "y" << k << "=" << y << endl;
+		k++;
+	}
+
+}
+
+
 int main()
 {
 	int n, m = 0;
@@ -13,8 +93,8 @@ int main()
 
 
 	cout << '\n';		//Introduction
-	cout << "============================================" << endl;
-	cout << setw(38) << "Function calculation application" << endl;
+	cout << "=============================================" << endl;
+	cout << "       Function calculation application      " << endl;
 	cout << "============================================" << endl;
 	cout << "Welcome to the program!" << endl;
 	cout << '\n';
@@ -25,74 +105,54 @@ int main()
 	for (;;) {
 		system("cls");		//Menu
 
-
-		cout << '\n' << "===========================================" << endl;
-		cout << setw(23) << "MENU" << endl;
+		cout << "\n===========================================" << endl;
+		cout << "                    MENU                    " << endl;
 		cout << "===========================================" << endl;
 		cout << "1. Calculate function for singular X." << endl;
 		cout << "2. Calculate function for a range of X." << endl;
 		cout << "3. Exit program." << endl;
-		cout << "Enter your choice:   ";
-		cin >> m;
 
-		if (cin.fail() || cin.peek() != '\n') {
-			cout << "Error, input number from 1 to 3!" << endl;
-			cin.clear();
-			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		try {
+			m = checkValidInt("Enter your choice: ");
+			if (m < 1 || m > 3) {
+				throw "Error: input correct data.";
+			}
+		}
+		catch (const char* ex) {
+			cout << ex << endl;
 			_getch();
 			continue;
 		}
-
 		switch (m) {
+
 		case 1:											//CASE 1 ------------
 			system("cls");
-			y = 0.0;
 			cout << "================================" << endl;
-			cout << setw(28) << "Calculating for singular X" << endl;
-			cout << "================================" << endl;
-			cout << "\n";
+			cout << "   Calculating for singular X   " << endl;//Getting n and x from user's input
+			cout << "================================\n" << endl;
 
-			cout << "Input n > 1, n = ";			//Getting n and x from user's input
-			do {
-				cin >> n;
-				if (cin.fail() || cin.peek() != '\n' || n < 2) {
-					cout << "Error, input correct n: ";
-					cin.clear();
-					cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			while (true) {
+				try {
+					n = checkValidInt("Input n > 1, n = ");
+					checkN(n);
+					break;
 				}
-			} while (cin.fail() || cin.peek() != '\n' || n < 2);
-
-
-			cout << "Input x = ";
-			while (!(cin >> x)) {
-				cout << "Error, input correct x : " << endl;
-				cin.clear();
-				cin.ignore(numeric_limits<streamsize>::max(), '\n');
+				catch (const char* ex) {
+					cout << ex << endl;
+				}
 			}
-
-			if (x < -3) {				//Function that calculates formula of domain and returns results
-				for (int i = 1; i <= n; i++) {
-					y += (pow(i + x, 2)) / (2.0 * i);
+			while (true) {
+				try {
+					x = checkValidDouble("Input x = ");
+					break;
+				}
+				catch (const char* ex) {
+					cout << ex << endl;
 				}
 			}
 
-			else {
-				double mlt = 1.0;
-
-				for (int j = 1; j <= n - 1; j++) {
-
-					double sum = 0.0;
-
-					for (int i = 2; i <= n + 1; i++) {
-
-						double temp = pow(i + (double)i / j - j, 2);
-						sum = sum + temp;
-					}
-					mlt = mlt * sum;
-				}
-				y = mlt;
-			}
-
+			//Function that calculates formula of domain and returns results
+			y = calcFunc1(n, x);
 			cout << "Results: x = " << x << endl;			//Outputting results
 			cout << setw(13) << "y = " << y << endl;
 			cout << '\n';
@@ -104,107 +164,65 @@ int main()
 			system("cls");
 			y = 0.0;
 			cout << "================================" << endl;
-			cout << setw(28) << "Calculating for a range of X" << endl;
-			cout << "================================" << endl;
-			cout << "\n";
-			cout << "Input n>1, n=";
+			cout << "  Calculating for a range of X  " << endl;
+			cout << "================================\n" << endl;
 
-			do {
-				cin >> n;
-				if (cin.fail() || cin.peek() != '\n' || n < 2) {
-					cout << "Error: Input correct n: ";
-					cin.clear();
-					cin.ignore(numeric_limits<streamsize>::max(), '\n');
-				}
-			} while (cin.fail() || cin.peek() != '\n' || n < 2);
-
-
-			cout << "Input start of range a: ";
-			do {
-				if (cin >> a) {
+			while (true) {
+				try {
+					n = checkValidInt("Input n > 1, n = ");
+					checkN(n);
 					break;
 				}
-				else {
-					cout << "Error: Input correct a:  ";
-					cin.clear();
-					cin.ignore(numeric_limits<streamsize>::max(), '\n');
+				catch (const char* ex) {
+					cout << ex << endl;
 				}
-			} while (true);
-
-
-			do {
-				cout << "Input end of range b: ";
-				if (!(cin >> b) || b < a) {
-					cout << "Error: Input correct b (b > a)." << endl;
-					cin.clear();
-					cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			}
+			while (true) {
+				try {
+					a = checkValidDouble("Input start of range a = ");
+					break;
 				}
-			} while (b < a || cin.fail());
-
-
-			do {
-				cout << "Input step for x (x>0): ";
-				cin >> step;
-				if (cin.fail()) {
-					cout << "Error: Input correct step." << endl;
-					cin.clear();
-					cin.ignore(numeric_limits<streamsize>::max(), '\n');
+				catch (const char* ex) {
+					cout << ex << endl;
 				}
-				else if (step <= 0) {
-					cout << "Error: Step must be bigger than 0." << endl;
+			}
+			while (true) {
+				try {
+					b = checkValidDouble("Input end of range b = ");
+					if (b <= a) {
+						throw "Error: b must be bigger than a!";
+					}
+					break;
 				}
-			
-			} while (step <= 0);
-
-			cout << '\n';			//Outputting results
-			cout << "------------------------------------------------" << endl;
-			cout << "CALCULATION RESULTS (n = " << n << "):" << endl;
+				catch (const char* ex) {
+					cout << ex << endl;
+				}
+			}
+			while (true) {
+				try {
+					step = checkValidDouble("Input step for x (step>0) = ");
+					checkRangeParams(a, b, step);
+					break;
+				}
+				catch (const char* ex) {
+					cout << ex << endl;
+				}
+			}
+			cout << "\n------------------------------------------------" << endl;
+			cout << "      CALCULATION RESULTS (n = " << n << "):      " << endl;
 			cout << "================================================" << endl;
 			cout << "X value" << setw(25) << "Y value" << endl;
 			cout << "------------------------------------------------" << endl;
-
-			int k = 1;		//Function that calculates formula of domain and returns results
-
-			for (double x = a; x <= b; x = x + step) {
-
-				double y = 0;
-
-				if (x < -3) {
-					for (int i = 1; i <= n; i++) {
-						y = y + (pow(i + x, 2)) / (2.0 * i);
-					}
-				}
-
-				else {
-					double mlt = 1.0;
-
-					for (int j = 1; j <= n - 1; j++) {
-
-						double sum = 0.0;
-
-						for (int i = 2; i <= n + 1; i++) {
-
-							double temp = pow(i + (double)i / j - j, 2);
-							sum = sum + temp;
-						}
-						mlt = mlt * sum;
-					}
-					y = mlt;
-				}
-				cout << fixed << setprecision(5);
-				cout << "x" << k << "=" << x << setw(15) << "y" << k << "=" << y << endl;
-				k++;
-			}
+			calcFunc2(n, a, b, step);
 			cout << '\n';
 			cout << "Press any key to return to the menu: ";
 			_getch();
 			break;
 		}
 		case 3:		//Case 3
-			cout << '\n' << "Exiting program..." << '\n' << endl;
-			cout << "Goodbye!" << endl;
+			cout << "\nExiting program...\n\n";
+			cout << "Goodbye!\n";
 			return 0;
 		}
 	}
-}											
-							//comeentrdgikdglgf
+}
